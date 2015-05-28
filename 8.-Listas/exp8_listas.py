b05091c79671afd3b781a8bfeb3df373c9a2fc32
Ian Mejias <ian.mejias@usach.cl>
2015-05-28 00:31:47 -0300

    exp8: se implementa funcion que pide valor de entrada
    La función además comprueba que la entrada cumpla con las condiciones
    establecidas del problema.

--- 8.-Listas/exp8_listas.py
+++ 8.-Listas/exp8_listas.py
@@ -3,8 +3,8 @@
 # AUTOR: Coordinación de Fundamentos de Computación y Programación
 # FECHA: Segundo Semestre de 2013
 #
-# Programa que recibe un número natural n, como entrada de un usuario a partir 
-# de éste se crea una lista con todos los números de 1 a n, eleva cada uno de 
+# Programa que recibe un número natural n, como entrada de un usuario a partir
+# de éste se crea una lista con todos los números de 1 a n, eleva cada uno de
 # los elementos al cubo, los invierte y los muestra por pantalla al usuario
 
 #
@@ -26,7 +26,7 @@ def crearLista(n):
 
 # Función que eleva al cubo cada elemento de una lista
 # Entrada: Una lista de numeros
-# Salida: Una nueva lista con todos los elementos de la primera, pero elevados 
+# Salida: Una nueva lista con todos los elementos de la primera, pero elevados
 # al cubo
 def elevarAlCubo(lista):
     result = []
@@ -63,6 +63,36 @@ def imprimirLista(lista):
 
     print text
 
+# Funcion que pide un número natural al usuario
+# Entrada: Un texto que se le muestra al usuario antes de esperar la entrada
+# por teclado.
+# Salida: El número que el usuario ingresa, y que cumple ademas con las
+# condiciones del problema
+def input_validado(mensaje):
+    incorrecto = True   # se asume que es incorrecto para empezar el ciclo
+    entrada = 0         # valor por defecto
+
+    while incorrecto:
+        entrada = input(mensaje)
+
+        if esNatural(entrada):
+            incorrecto = False
+        else:
+            print "\nLa entrada \"%s\" es incorrecta!" % str(entrada)
+            #print "\nEntrada incorrecta!"
+
+    return entrada
+
+# Funcion que dice si un numero es natural o no.
+# Entrada: Un numero
+# Salida: True si el numero es natural, y False en caso contrario
+def esNatural(num):
+    if num > 0 and int(num) == num:
+        return True
+
+    else:
+        return False
+
 
 # BLOQUE PRINCIPAL
 #
@@ -70,22 +100,19 @@ def imprimirLista(lista):
 # Se solicita el ingreso de n y se valida que sea un número
 # natural, en caso de que no sea se pregunta nuevamente
 
-n = -1
-
-while n < 0:
-    n = input("Ingrese un número natural: ")
+n = input_validado("Ingrese un número natural: ")
 
     # PROCESAMIENTO
 # Se crea la lista, posteriormente se eleva cada uno de los elementos al
 # cubo para finalmente invertir el orden de los elementos de la lista
 lista = crearLista(n)
 
-elevarAlCubo(lista)
-invertirLista(lista)
+alCubo = elevarAlCubo(lista)
+invertida = invertirLista(alCubo)
 
     # SALIDA
 # Se imprime la salida al usuario
-imprimirLista(lista)
+imprimirLista(invertida)
 
 
 
